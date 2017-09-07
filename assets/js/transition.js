a45c5882364cabb4a7c1d28c71e966ee416da76b
_lowi <me@lowi.org>
2017-09-07 22:06:04 +0200

    transitions popitions

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -1,19 +1,33 @@
 import anime from 'animejs'
 
+function _href(e) {
+  if(e.tagName.match(/body/gi)) return null
+  let location = e.getAttribute('href')
+  if(location) return location
+  else return _href(e.parentElement)}
+
+function _transitionTo(doneFn){
+  let τ = document.getElementById('transition'),
+      δ = _.sample(['left', 'top']),
+      ε = _.sample(['linear', 'easeInQuad', 'easeInCubic', 'easeInQuart', 'easeInQuint', 'easeInSine', 'easeInExpo', 'easeInCirc', 'easeInBack', 'easeOutQuad', 'easeOutCubic', 'easeOutQuart', 'easeOutQuint', 'easeOutSine', 'easeOutExpo', 'easeOutCirc', 'easeOutBack', 'easeInOutQuad', 'easeInOutCubic', 'easeInOutQuart', 'easeInOutQuint', 'easeInOutSine', 'easeInOutExpo', 'easeInOutCirc', 'easeInOutBack']),
+      ω = { targets: '#transition',
+            top: 0,
+            left: 0,
+            easing:   ε,
+            duration: 420 },
+      α  = anime(ω) 
+  α.complete = () => doneFn()
+ }
+
 function init() {
   console.log('init transition')
 
   // global function for navigating a step back
   // used on single pages to navigate back to the home screen
   window.goBack = function() {
-    window.history.back()
+    
+    _transitionTo(() => {window.history.back()})
   }
-  
-  function _href(e) {
-    if(e.tagName.match(/body/gi)) return null
-    let location = e.getAttribute('href')
-    if(location) return location
-    else return _href(e.parentElement)}
   
   // capture all a#href clicks
   window.onclick = e => { 
@@ -21,16 +35,8 @@ function init() {
   
     if(location) {
       e.preventDefault()
-      let τ = document.getElementById('transition'),
-          δ = _.sample(['left', 'top']),
-          ε = _.sample(['linear', 'easeInQuad', 'easeInCubic', 'easeInQuart', 'easeInQuint', 'easeInSine', 'easeInExpo', 'easeInCirc', 'easeInBack', 'easeOutQuad', 'easeOutCubic', 'easeOutQuart', 'easeOutQuint', 'easeOutSine', 'easeOutExpo', 'easeOutCirc', 'easeOutBack', 'easeInOutQuad', 'easeInOutCubic', 'easeInOutQuart', 'easeInOutQuint', 'easeInOutSine', 'easeInOutExpo', 'easeInOutCirc', 'easeInOutBack']),
-          ω = { targets: '#transition',
-                top: 0,
-                left: 0,
-                easing:   ε,
-                duration: 420 },
-          α  = anime(ω) 
-    α.complete = () => window.location = location }}
+      _transitionTo(() => {window.location = location})
+    }}
 
   let τ = document.getElementById('transition'),
       δ = _.sample(['left', 'top']),
